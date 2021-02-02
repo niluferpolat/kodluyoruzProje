8d84994f97515153ab4c54898131d1cf0595cebc
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-02-02 19:45:17 +0300

    added blogHeader

--- pet-app-fe/src/blog/blog.js
+++ pet-app-fe/src/blog/blog.js
@@ -1,28 +1,29 @@
-import { useDispatch } from "react-redux"
-import { useEffect } from "react"
-import { fetchPosts } from "../actions/post"
-import { List } from "antd"
-import { useSelector } from "react-redux"
-import Post from "./post"
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchPosts } from '../actions/post';
+import { List } from 'antd';
+import { useSelector } from 'react-redux';
+import Post from './post';
+import './blog.css';
 const Blog = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchPosts());
-    }, [dispatch])
-    const posts = useSelector((state) => state.posts.posts);
-    return (<>
-        <List grid={{ gutter: 16, column: 4 }}
-            dataSource={posts}
-            renderItem={item => (
-                <List.Item>
-                    <Post {...item} />
-                </List.Item>
-            )}
-        />
-
-
-
-
-    </>)
-}
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchPosts());
+	}, [dispatch]);
+	const posts = useSelector((state) => state.posts.posts);
+	return (
+		<>
+			<div className="blogHeader"></div>
+			<List
+				grid={{ gutter: 16, column: 4 }}
+				dataSource={posts}
+				renderItem={(item) => (
+					<List.Item>
+						<Post {...item} />
+					</List.Item>
+				)}
+			/>
+		</>
+	);
+};
 export default Blog;
--- pet-app-fe/src/blog/post.js
+++ pet-app-fe/src/blog/post.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Card, Button, Typography } from 'antd';
 import './post.css';
 
+
 const Post = ({ _id, title, content, image, createdAt }) => {
 	const convertRelativeTime = (date) => {
 		return moment(date).fromNow();
@@ -10,25 +11,28 @@ const Post = ({ _id, title, content, image, createdAt }) => {
 	const { Meta } = Card;
 	const { Text } = Typography;
 	return (
-		<div className="all">
-			<Card className="card" hoverable>
-				<img className="imgStyle" src={image} height="300px" alt="" />{' '}
-				<div className="contentStyle">
-					<Text className="time">{convertRelativeTime(createdAt)}</Text>
-					<br />
-					<Text className="title" strong>
-						{title}
-					</Text>
-					<hr />
-					<Meta className="description" description={content?.substring(0, 90) + '...'} />
-					<Button type="link">
-						<Link className="link" to={`/posts/${_id}`}>
-							Daha fazla...
-						</Link>
-					</Button>
-				</div>
-			</Card>
-		</div>
+		<>
+			<div className="all">
+				<Card className="card" hoverable>
+					<img className="imgStyle" src={image} height="300px" alt="" />{' '}
+					<div className="contentStyle">
+						<Text className="time">{convertRelativeTime(createdAt)}</Text>
+						<br />
+						<Text className="title" strong>
+							{title}
+						</Text>
+						<hr />
+						<Meta className="description" description={content?.substring(0, 90) + '...'} />
+						<Button type="link">
+							<Link className="link" to={`/posts/${_id}`}>
+								Daha fazla...
+							</Link>
+						</Button>
+					</div>
+				</Card>
+			</div>
+			</>
+		
 	);
 };
 export default Post;
