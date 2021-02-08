3afc4350027868ef1edbb81dbee8900f69ee794f
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-02-08 05:57:03 +0300

    added button design

--- pet-app-fe/src/adopt/Adoption.js
+++ pet-app-fe/src/adopt/Adoption.js
@@ -1,4 +1,3 @@
-import { PlusCircleOutlined } from '@ant-design/icons';
 import { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAdopt } from '../actions/adopt';
@@ -23,30 +22,28 @@ const Adoption = () => {
 	const adopts = useSelector((state) => state.adopts.adopts);
 	return (
 		<>
-			<div className="adoptHeader"></div>
+			<div className="adoptHeader">
+				{!isAuthenticated() && (
+					<Fragment>
+						<Button className="btn_Sahiplendir" type="primary" onClick={warning}>
+							Yeni Sahiplendirme İlanı
+						</Button>
+					</Fragment>
+				)}
+				{isAuthenticated() && (
+					<Fragment>
+						<Link to="/adoption/addAdopt">
+							<Button className="btn_Sahiplendir" type="primary">
+								Sahiplendir
+							</Button>
+						</Link>
+					</Fragment>
+				)}
+			</div>
+			<Divider />
 			<div className="adoptBody">
 				<Content className="site-layout" style={{ padding: '0 50px' }}>
 					<div className="site-layout-background">
-						{!isAuthenticated() && (
-							<Fragment>
-								<Button type="primary" onClick={warning} style={{ float: 'right' }}>
-									Yeni Sahiplendirme İlanı
-									<PlusCircleOutlined />
-								</Button>
-							</Fragment>
-						)}
-						{isAuthenticated() && (
-							<Fragment>
-								<Link to="/adoption/addAdopt">
-									<Button type="primary" style={{ float: 'right' }}>
-										Yeni Sahiplendirme İlanı
-										<PlusCircleOutlined />
-									</Button>
-								</Link>
-							</Fragment>
-						)}
-
-						<Divider />
 						<List
 							className="listItems"
 							grid={{ gutter: 16, column: 4 }}
