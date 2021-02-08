8d53b9d31d0fc1fcb3ecb9888f06a25e4a9d3621
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-02-08 17:35:58 +0300

    added sidebar css

--- pet-app-fe/src/adopt/AdoptionDetails.js
+++ pet-app-fe/src/adopt/AdoptionDetails.js
@@ -1,117 +1,156 @@
 import { useEffect, Fragment, useState } from 'react';
 import moment from 'moment';
-import { DeleteTwoTone, EditTwoTone, PhoneOutlined, MailOutlined, ExclamationCircleOutlined, EnvironmentOutlined } from '@ant-design/icons';
+import {
+	DeleteTwoTone,
+	EditTwoTone,
+	PhoneOutlined,
+	MailOutlined,
+	ExclamationCircleOutlined,
+	EnvironmentOutlined,
+} from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { isAuthenticated } from '../helpers/auth';
 import { fetchSingleAdopt, deleteAdopt } from '../actions/adopt';
 import { Typography, Divider, Button, Modal, Layout } from 'antd';
-import EditAdoptForm from "./EditAdoptForm"
+import EditAdoptForm from './EditAdoptForm';
+import "./AdoptionDetails.css";
 const { Title, Paragraph, Text } = Typography;
 const { confirm } = Modal;
 const PostDetails = ({ history, match }) => {
-    const { id } = match.params;
-    const currentAdopt = useSelector((state) => state.adopts.currentAdopts);
-    const [editMode, setEditMode] = useState(false);
-    const { Content } = Layout;
-    const openEditMode = () => {
-        setEditMode(true);
-    };
-    const closeEditMode = () => {
-        setEditMode(false);
-    };
-    const convertRelativeTime = (date) => {
-        return moment(date).fromNow();
-    };
-    function info() {
-        Modal.info({
-            title: 'Ulaşım Bilgileri',
-            content: (
-                <div>
-                    <p>{currentAdopt?.communication}</p>
+	const { id } = match.params;
+	const currentAdopt = useSelector((state) => state.adopts.currentAdopts);
+	const [editMode, setEditMode] = useState(false);
+	const { Content } = Layout;
+	const openEditMode = () => {
+		setEditMode(true);
+	};
+	const closeEditMode = () => {
+		setEditMode(false);
+	};
+	const convertRelativeTime = (date) => {
+		return moment(date).fromNow();
+	};
+	function info() {
+		Modal.info({
+			title: 'Ulaşım Bilgileri',
+			content: (
+				<div>
+					<p>{currentAdopt?.communication}</p>
+				</div>
+			),
+			onOk() {},
+		});
+	}
+	function showConfirm() {
+		confirm({
+			title: 'Bu makaleyi silmek istediğinizden emin misiniz?',
+			icon: <ExclamationCircleOutlined />,
+			okText: 'Evet',
+			cancelText: 'İptal',
+			onOk() {
+				dispatch(deleteAdopt(currentAdopt?._id));
+				history.push('/adoption');
+			},
+			onCancel() {
+				console.log('Cancel');
+			},
+		});
+	}
 
-                </div>
-            ),
-            onOk() { },
-        });
-    }
-    function showConfirm() {
-        confirm({
-            title: 'Bu makaleyi silmek istediğinizden emin misiniz?',
-            icon: <ExclamationCircleOutlined />,
-            okText: 'Evet',
-            cancelText: 'İptal',
-            onOk() {
-                dispatch(deleteAdopt(currentAdopt?._id));
-                history.push('/adoption');
-            },
-            onCancel() {
-                console.log('Cancel');
-            },
-        });
-    }
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchSingleAdopt(id));
+	}, [id, dispatch]);
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchSingleAdopt(id));
-    }, [id, dispatch]);
-
-    return (
-        <Content className="site-layout" style={{ padding: '0 50px' }}>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 500 }}>
-                <Typography>
-                    {editMode ? (
-                        <EditAdoptForm adopt={currentAdopt} closeEditMode={closeEditMode} />
-                    ) : (
-                            <div>
-                                { isAuthenticated() && (
-                                    <Fragment>
-                                        <div className="btns">
-                                            <Button
-                                                type="link"
-                                                style={{
-                                                    float: 'right',
-                                                    fontSize: '20px',
-                                                    borderBlockColor: 'white',
-                                                    color: 'white',
-                                                }}
-                                                onClick={showConfirm}
-                                            >
-                                                <DeleteTwoTone twoToneColor="#ee99a0" />
-                                                <Text type="secondary">Sil</Text>
-                                            </Button>
-                                            <Button
-                                                type="link"
-                                                onClick={openEditMode}
-                                                style={{
-                                                    float: 'right',
-                                                    fontSize: '20px',
-                                                    borderBlockColor: 'white',
-                                                    color: 'white',
-                                                }}
-                                            >
-                                                <EditTwoTone twoToneColor="#ee99a0" />
-                                                <Text type="secondary">Düzenle</Text>
-                                            </Button>
-                                        </div>
-                                    </Fragment>
-                                )}
-                                <Text>{convertRelativeTime(currentAdopt?.createdAt)}</Text>
-                                <Text style={{ marginLeft: "380px", fontSize: "30px" }} strong>{currentAdopt?.title}</Text>
-                                <Divider />
-                                <img style={{ marginLeft: "400px" }} width={400} height={400} src={currentAdopt?.image} />
-                                <br />
-                                <Paragraph style={{ marginLeft: "400px", marginTop: "10px" }}>{currentAdopt?.content}</Paragraph>
-                                <br />
-                                <Text style={{ marginLeft: "400px", marginTop: "10px" }}><EnvironmentOutlined />{currentAdopt?.town}/{currentAdopt?.province}</Text>
-                                <br />
-                                <Text style={{ marginLeft: "400px", marginTop: "10px" }}><strong>Cinsiyeti: </strong>{currentAdopt?.gender}</Text><br />
-                                <Text style={{ marginLeft: "400px", marginTop: "10px" }}><strong>Yaşı: </strong>{currentAdopt?.age}</Text><br />
-                                <Text style={{ marginLeft: "400px", marginTop: "10px" }}><strong>Türü: </strong>{currentAdopt?.species}</Text><br />
-                                <Button type="primary" size="large" onClick={info} style={{ marginLeft: "560px", marginTop: "10px" }}>Ulaşın</Button>
-                            </div>)}
-                </Typography>
-            </div>
-        </Content>
-    );
+	return (
+		<Content className="site-layout" style={{ padding: '0 50px' }}>
+			<div className="site-layout-background" style={{ padding: 24, minHeight: 500 }}>
+				<Typography>
+					{editMode ? (
+						<EditAdoptForm adopt={currentAdopt} closeEditMode={closeEditMode} />
+					) : (
+						<div>
+							{isAuthenticated() && (
+								<Fragment>
+									<div className="btns">
+										<Button
+											type="link"
+											style={{
+												float: 'right',
+												fontSize: '20px',
+												borderBlockColor: 'white',
+												color: 'white',
+											}}
+											onClick={showConfirm}
+										>
+											<DeleteTwoTone twoToneColor="#ee99a0" />
+											<Text type="secondary">Sil</Text>
+										</Button>
+										<Button
+											type="link"
+											onClick={openEditMode}
+											style={{
+												float: 'right',
+												fontSize: '20px',
+												borderBlockColor: 'white',
+												color: 'white',
+											}}
+										>
+											<EditTwoTone twoToneColor="#ee99a0" />
+											<Text type="secondary">Düzenle</Text>
+										</Button>
+									</div>
+								</Fragment>
+							)}
+							<Text>{convertRelativeTime(currentAdopt?.createdAt)}</Text>
+							<Text style={{ marginLeft: '380px', fontSize: '30px' }} strong>
+								{currentAdopt?.title}
+							</Text>
+							<Divider />
+							<img
+								style={{ marginLeft: '400px' }}
+								width={400}
+								height={400}
+								src={currentAdopt?.image}
+							/>
+							<br />
+							<Paragraph style={{ marginLeft: '400px', marginTop: '10px' }}>
+								{currentAdopt?.content}
+							</Paragraph>
+							<br />
+							<Text style={{ marginLeft: '400px', marginTop: '10px' }}>
+								<EnvironmentOutlined />
+								{currentAdopt?.town}/{currentAdopt?.province}
+							</Text>
+							<br />
+							<Text style={{ marginLeft: '400px', marginTop: '10px' }}>
+								<strong>Cinsiyeti: </strong>
+								{currentAdopt?.gender}
+							</Text>
+							<br />
+							<Text style={{ marginLeft: '400px', marginTop: '10px' }}>
+								<strong>Yaşı: </strong>
+								{currentAdopt?.age}
+							</Text>
+							<br />
+							<Text style={{ marginLeft: '400px', marginTop: '10px' }}>
+								<strong>Türü: </strong>
+								{currentAdopt?.species}
+							</Text>
+							<br />
+							<Button
+								type="primary"
+								size="large"
+								onClick={info}
+								style={{ marginLeft: '560px', marginTop: '10px' }}
+							>
+								Ulaşın
+							</Button>
+						</div>
+					)}
+				</Typography>
+			</div>
+		</Content>
+	);
 };
 export default PostDetails;
--- pet-app-fe/src/components/Sidebar.js
+++ pet-app-fe/src/components/Sidebar.js
@@ -1,53 +1,59 @@
-import "./App.css"
-import { Layout, Menu } from 'antd';
-import { Link, Switch, Route } from "react-router-dom"
+import './App.css';
+import { Layout, Menu, Divider } from 'antd';
+import { Link, Switch, Route } from 'react-router-dom';
 import { SnippetsOutlined } from '@ant-design/icons';
-import React, { Component } from "react"
-import addArticle from "../blog/addArticle";
-import Blog from "../blog/blog";
-import PostDetails from "../blog/postdetails"
+import React, { Component } from 'react';
+import addArticle from '../blog/addArticle';
+import Blog from '../blog/blog';
+import PostDetails from '../blog/postdetails';
+import './Sidebar.css';
 const { Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 class SideBar extends Component {
-    state = {
-        collapsed: false,
-    };
+	state = {
+		collapsed: false,
+	};
 
-    onCollapse = collapsed => {
-        console.log(collapsed);
-        this.setState({ collapsed });
-    };
+	onCollapse = (collapsed) => {
+		console.log(collapsed);
+		this.setState({ collapsed });
+	};
 
-    render() {
-        const { collapsed } = this.state;
-        return (
-            <Layout style={{ minHeight: '100vh' }} >
-                <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} className="sider">
-                    <div className="logo" />
-                    <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
-                        <SubMenu key="sub1" icon={<SnippetsOutlined />} title="Blog">
-                            <Menu.Item key="/add_article">Yeni Yazı<Link to="/add_article" /></Menu.Item>
-                            <Menu.Item key="/posts">Blog<Link to="/posts" /></Menu.Item>
-
-                        </SubMenu>
-                    </Menu>
-                </Sider>
-                <Layout className="site-layout">
-                    <Content style={{ margin: '0 16px' }}>
-
-                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                            <Switch>
-                                <Route path="/add_article" component={addArticle}></Route>
-                                <Route exact path="/posts" component={Blog}></Route>
-                                <Route exact path="/posts/:id" component={PostDetails} />
-                            </Switch>
-                        </div>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>Petstagram ©2020 Created by IEN</Footer>
-                </Layout>
-            </Layout>
-        );
-    }
+	render() {
+		const { collapsed } = this.state;
+		return (
+			<Layout style={{ minHeight: '100vh' }}>
+				<Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} className="sider">
+					<div className="logo" />
+					<Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
+						<SubMenu key="sub1" icon={<SnippetsOutlined />} title="Blog">
+							<Menu.Item key="/add_article">
+								Yeni Yazı
+								<Link to="/add_article" />
+							</Menu.Item>
+							<Menu.Item key="/posts">
+								Blog
+								<Link to="/posts" />
+							</Menu.Item>
+						</SubMenu>
+					</Menu>
+				</Sider>
+				<Layout className="site-layout">
+					<Content className="content">
+						<div className="site-layout-background">
+							<Switch>
+								<Route path="/add_article" component={addArticle}></Route>
+								<Route exact path="/posts" component={Blog}></Route>
+								<Route exact path="/posts/:id" component={PostDetails} />
+							</Switch>
+						</div>
+					</Content>
+					<Divider />
+					<Footer className="footer">Petstagram ©2020 Created by IN</Footer>
+				</Layout>
+			</Layout>
+		);
+	}
 }
 export default SideBar;
