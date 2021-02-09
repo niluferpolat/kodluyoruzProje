c1f9ec0d4253f12b63016b51195939f6b062fc57
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-02-10 01:07:37 +0300

    fixed some problems

--- pet-app-fe/src/Home/Home.js
+++ pet-app-fe/src/Home/Home.js
@@ -1,6 +1,6 @@
 import './Home.css';
-import { useState } from "react"
-import { Link, withRouter } from "react-router-dom"
+import { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 const Home = ({ history }) => {
 	const handleSignin = () => {
 		history.push('/signin');
@@ -11,22 +11,38 @@ const Home = ({ history }) => {
 				<source src="/Video/home-video.mp4" type="video/mp4" />
 			</video>
 			<div className="Home">
-				<Link className="item" exact to="/" >Anasayfa</Link>
+				<ul>
+					<Link className="item-home" exact to="/">
+						Anasayfa
+					</Link>
+				</ul>
 				<hr />
-				<Link to="/adoption">Sahiplen</Link>
+				<ul>
+					<Link className="item-home" to="/adoption">
+						Sahiplen
+					</Link>
+				</ul>
 				<hr />
-				<Link to="/map">En Yakın Veteriner</Link>
+				<ul>
+					<Link className="item-home" to="/map">
+						En Yakın Veteriner
+					</Link>
+				</ul>
 				<hr />
-				<Link href="/posts">Blog</Link>
+				<ul>
+					<Link className="item-home" href="/posts">
+						Blog
+					</Link>
+				</ul>
 				<hr />
-				<button className="btn" onClick={handleSignin}>
-					Giriş <i className="fas fa-paw"></i>
-				</button>
+				<ul>
+					<button className="btn-home" onClick={handleSignin}>
+						Giriş <i className="fas fa-paw"></i>
+					</button>
+				</ul>
 			</div>
-
 		</div>
 	);
-}
+};
 
 export default withRouter(Home);
-
