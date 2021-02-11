78f632563c3d848a1c9e85e5c1281557b3932a6c
Nilüfer Polat <niluferpolat2@gmail.com>
2021-02-12 00:57:44 +0300

    added login and logout buttons according to authentication

--- pet-app-fe/src/Home/Home.js
+++ pet-app-fe/src/Home/Home.js
@@ -1,8 +1,15 @@
 import './Home.css';
+import { isAuthenticated, logout } from '../helpers/auth'
 import { Link, withRouter } from 'react-router-dom';
+import { Fragment } from "react"
 const Home = ({ history }) => {
 	const handleSignin = () => {
 		history.push('/signin');
+	};
+	const handleLogout = (event) => {
+		logout(() => {
+			history.push('/signin');
+		});
 	};
 	return (
 		<div id="home">
@@ -34,11 +41,25 @@ const Home = ({ history }) => {
 					</Link>
 				</ul>
 				<hr />
-				<ul className="home-ul">
-					<button className="btn-home" onClick={handleSignin}>
-						Giriş <i className="fas fa-paw" />
-					</button>
-				</ul>
+				{!isAuthenticated() && (
+					<Fragment>
+						<ul className="home-ul">
+							<button className="btn-home" onClick={handleSignin}>
+								Giriş <i className="fas fa-paw" />
+							</button>
+						</ul>
+					</Fragment>
+				)}
+				{isAuthenticated() && (
+					<Fragment>
+						<ul className="home-ul">
+							<button className="btn-home" onClick={handleLogout}>
+								Çıkış <i className="fas fa-paw" />
+							</button>
+						</ul>
+					</Fragment>
+				)}
+
 			</div>
 		</div>
 	);
