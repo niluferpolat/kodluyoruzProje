dbf8adf8f22ebca8fa7530db9f15c93027ea448f
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-02-12 07:28:34 +0300

    done vet styles

--- pet-app-fe/src/Veterinary/List.js
+++ pet-app-fe/src/Veterinary/List.js
@@ -1,33 +1,40 @@
-import React from "react";
-import Map from "./Map";
+import React from 'react';
+import { Card } from 'antd';
+import Map from './Map';
 
 class List extends React.Component {
-    render() {
-        // location list
-        const locations = this.props.locations;
-        return (
-            <div id="list">
-                <h2>Locations</h2>
-                <p>{this.props.queryString}</p>
-                <input type="text" value={this.props.queryString} onChange={e => this.props.handleChange(e.target.value)} />
-                <ol>
-                    {locations.map(loc => (
-                        <li key={loc.venue.id}>
-                            <div>
-                                <p className="title">
-                                    Name:{" "}
-                                    <a href="#" onClick={() => this.props.showInfoContent(loc)}>
-                                        {loc.venue.name}
-                                    </a>
-                                </p>
-                                <p>Address: {loc.venue.location.address}</p>
-                            </div>
-                        </li>
-                    ))}
-                </ol>
-            </div>
-        );
-    }
+	render() {
+		// location list
+		const locations = this.props.locations;
+		return (
+			<div id="list">
+				<h2 className="vet-title">Locations</h2>
+				<p>{this.props.queryString}</p>
+				<input
+					className="vet-input"
+					type="text"
+					value={this.props.queryString}
+					onChange={(e) => this.props.handleChange(e.target.value)}
+				/>
+				<ol>
+					{locations.map((loc) => (
+						<li key={loc.venue.id}>
+							<Card className="vet-list">
+								<span className="vet-name-title">Veteriner Adı: </span>
+								<a className="vet-name" href="#" onClick={() => this.props.showInfoContent(loc)}>
+									{loc.venue.name}
+								</a>
+								<br />
+								<br />
+								<span className="vet-adres-title">Adresi:</span>
+								<span className="vet-adres"> {loc.venue.location.address}</span>
+							</Card>
+						</li>
+					))}
+				</ol>
+			</div>
+		);
+	}
 }
 
 export default List;
--- pet-app-fe/src/Veterinary/Veterinary.js
+++ pet-app-fe/src/Veterinary/Veterinary.js
@@ -1,45 +1,43 @@
-import React, { Component } from "react";
-import Navbar from "../components/Navbar";
-import "./Veterinary.css";
-import Content from "./Content"
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import './Veterinary.css';
+import Content from './Content';
 class Veterinary extends Component {
-    initMap() {
-        let map = new window.google.maps.Map(document.getElementById("map"), {
-            center: { lat: 41.1558, lng: 27.8137 },
-            zoom: 12
-        });
-        window.mapObject = map;
-    }
+	initMap() {
+		let map = new window.google.maps.Map(document.getElementById('map'), {
+			center: { lat: 41.1558, lng: 27.8137 },
+			zoom: 12,
+		});
+		window.mapObject = map;
+	}
 
-    loadScript() {
-        let scriptEl = this.createMapScript();
-        let scriptsOnPage = document.getElementsByTagName("script");
-        let script = scriptsOnPage[0];
-        console.log(scriptsOnPage);
-        console.log(script.parentNode);
-        script.parentNode.insertBefore(scriptEl, script);
-        window.initMap = this.initMap;
-    }
+	loadScript() {
+		let scriptEl = this.createMapScript();
+		let scriptsOnPage = document.getElementsByTagName('script');
+		let script = scriptsOnPage[0];
+		console.log(scriptsOnPage);
+		console.log(script.parentNode);
+		script.parentNode.insertBefore(scriptEl, script);
+		window.initMap = this.initMap;
+	}
 
-    createMapScript() {
-        let mapScript = document.createElement("script");
-        const API_KEY = "AIzaSyCj9NkidHq86NCNFLOrZw_wZsFmrpDQFAY";
-        mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
-        mapScript.async = true;
-        mapScript.defer = true;
-        return mapScript;
-    }
-    render() {
-        this.loadScript();
-        return (
-            <div>
-                <Navbar />
-                <div className="App" />
-                <Content />
-            </div>
-
-        );
-    }
+	createMapScript() {
+		let mapScript = document.createElement('script');
+		const API_KEY = 'AIzaSyCj9NkidHq86NCNFLOrZw_wZsFmrpDQFAY';
+		mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+		mapScript.async = true;
+		mapScript.defer = true;
+		return mapScript;
+	}
+	render() {
+		this.loadScript();
+		return (
+			<>
+				<Navbar />
+				<Content />
+			</>
+		);
+	}
 }
 
 export default Veterinary;
